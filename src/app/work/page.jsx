c866c0e3ac02e6c9c9187426fcf642dad8028111
BBQ love 69 <74670055+zaw-creator@users.noreply.github.com>
2025-07-14 23:43:23 +0100

    added the github contribution chart

--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -1,8 +1,17 @@
+import GitHubCalendar from 'react-github-calendar';
+
 export default function Work() {
     return (
         <div style={{ textAlign: "center"}}>
             <h1>Work Page</h1>
+            <GitHubCalendar
+                username="zaw-creator"
+                style={{ maxWidth: "700px", margin: "0 auto" }}
+            />
             <p>This is the work page of my minimalist portfolio.</p>
+
+ 
+
         </div>
     );
 }
